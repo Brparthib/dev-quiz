9f2ed52bb61caeeb07774c157de25db9af0f9236
ParthibBr <bparthib77@gmail.com>
2023-06-02 20:18:38 +0600

    topics button setup

--- src/components/Topic/Topic.jsx
+++ src/components/Topic/Topic.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import './Topic.css'
 
 const Topic = ({ topic }) => {
+    const {id, name, logo} = topic
   return (
     <div className="topic">
       <div className="card">
-        <img src={topic.logo} className={`card-img-top ${topic.id == 5 ? "bg-dark" : "bg-light"}`} alt="img" />
+        <img src={logo} className={`card-img-top ${id == 5 ? "bg-dark" : "bg-light"}`} alt="img" />
         <div className="card-body">
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+          <p>{name}</p>
+          <button className="btn">{'Start quiz->'}</button>
         </div>
       </div>
     </div>
