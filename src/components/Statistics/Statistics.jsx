9c44ebff4c95fac895800b684bf900a4f15fc1ae
ParthibBr <bparthib77@gmail.com>
2023-06-09 21:18:19 +0600

    all complete

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Statistics.css'
 import {
   LineChart,
   Line,
@@ -11,13 +12,73 @@ import {
 } from "recharts";
 
 const Statistics = () => {
+
+    const data = [
+        {
+          name: 'Page A',
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Page B',
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'Page C',
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Page D',
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'Page E',
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Page F',
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Page G',
+          uv: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
+
   return (
-    <div>
-        <div>
-            <h2 className="text-center my-3">Statistics</h2>
+    <div className="statistics">
+      <div>
+        <h2 className="text-center my-3">Statistics</h2>
+      </div>
+      <div className="card">
+        <div className="card-body">
+        <LineChart
+          width={650}
+          height={450}
+          data={data}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
         </div>
-      <div class="card">
-        <div class="card-body">This is some text within a card body.</div>
       </div>
     </div>
   );
